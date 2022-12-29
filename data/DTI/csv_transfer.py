50b0575b67c1dfb737edb1fb588b7a06e4048aca
zz <64080766+zznb123@users.noreply.github.com>
2022-12-29 19:00:42 +0800

    Delete data directory

--- data/DTI/csv_transfer.py
+++
@@ -1,9 +0,0 @@
-ls = open('biosnap.tsv').readlines()
-bio = ""
-for line in ls:
-    bio = bio + ",".join(line.split()) + "\n"
-print(bio)
-
-f0 = open("bios.csv", "x")
-f0.write(bio)
-f0.close()
--- data/DTI/matrix.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-f1 = open('dtifold1.csv','rb')
-dtifold_t = np.loadtxt(f1, delimiter=' ',skiprows=1)
-f1.close()
-dtifold_m = np.array(dtifold_t)
